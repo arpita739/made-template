c7b4a5dfc2f8867847489f52b3af1791e4456c4f
vi55zebe <arpita.halder@fau.de>
2023-11-12 16:21:18 +0100

    final code

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,31 +1,24 @@
 import pandas as pd
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine
+from io import StringIO
+import urllib.request
 
-# Step 1: Download the CSV file
+#Step 1 : Downloading the CSV file
 url = 'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv'
-df = pd.read_csv(url,sep=';')
+response = urllib.request.urlopen(url)
+csv_content = response.read().decode('utf-8')
 
-# Step 2: Analyze DataFrame and map pandas data types to SQLite types
-sqlite_type_mapping = {
-    'object': 'TEXT',
-    'int64': 'INTEGER',
-    'float64': 'REAL',
-}
+# Step 2: Reading the CSV file into a pandas DataFrame
+df = pd.read_csv(StringIO(csv_content),sep=';')
 
-column_data_types = {col: sqlite_type_mapping[str(df[col].dtype)] for col in df.columns}
-
-# Step 3: Define the SQLite database connection and create an SQLAlchemy engine
+# Step 3: Defining the SQLite database connection and create an SQLAlchemy engine
 db_connection_str = 'sqlite:///airports.sqlite'
 engine = create_engine(db_connection_str)
 
-# Step 4: Write the DataFrame to the SQLite database with assigned types
-df.to_sql('airports', con=engine, index=False, if_exists='replace', dtype=column_data_types)
+# Step 4: Writing the DataFrame to the SQLite database
+df.to_sql('airports', con=engine, index=False, if_exists='replace')
 
-# Optional: Print the defined column data types
-inspector = inspect(engine)
-table_columns = inspector.get_columns('airports')
-for column in table_columns:
-    print(f"Column: {column['name']}, Type: {column['type']}")
 
-# Step 5: Close the engine
+
+# Close the engine
 engine.dispose()
